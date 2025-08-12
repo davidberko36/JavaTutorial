b5f09df6dc08976557abffbd5fca17ed5ab511a5
David Berko <daberko364@gmail.com>
2025-08-12 12:41:29 +0000

    initial commit

---
+++ hello.java
@@ -0,0 +1,16 @@
+public class hello {
+
+
+    class Boy{
+        public  void printKofi(){
+            System.out.println("Kofi");
+        }
+    }
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+        int numberOne = 4;
+        double doubleOne = 4.5;
+        boolean boolOne = true;
+        char charOne = 'e'; 
+    }
+}
